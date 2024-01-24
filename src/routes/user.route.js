35dbcc3788bbe1b14c0b91a185673f1d2786d648
avengsum <sy710481@gmail.com>
2024-01-25 01:46:21 +0530

    all set to work on registerUser route

---
+++ src/routes/user.route.js
@@ -0,0 +1,8 @@
+import {Router, } from "express";
+import { registerUser } from "../controllers/user.controller";
+
+const Router = Router();
+
+Router.route('/register').post(registerUser)
+
+export default Router
